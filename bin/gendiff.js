574685fbb1644c9b1fd8278fbe590a2547f0d3d0
GriDan <ddd1444ddd@gmail.com>
2025-07-23 20:39:09 +0500

    add gendiff.js

---
+++ bin/gendiff.js
@@ -0,0 +1,8 @@
+import {Command} from "commander";
+
+const program = new Command();
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .option('-V, --version', 'output the version number')
+    .option('-h, --help', 'display help for command')
