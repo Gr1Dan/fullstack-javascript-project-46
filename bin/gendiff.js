86fe1594418a22867e2ca12f68315a53ab85294c
GriDan <ddd1444ddd@gmail.com>
2025-08-16 20:03:19 +0500

    third step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,26 +2,15 @@
 
 import { Command } from "commander";
 
+
 const program = new Command();
 
 program
     .option(`-V, --version`, `output the version number`)
     .option(`-f, --format [type]`, `output format`)
-    .option(`-h, --help`, `displays help for command`);
+    .option(`-h, --help`, `displays help for command`)
+    .argument('<filepath1>', 'path to file1')
+    .argument('<filepath2>', 'path to file2')
 
-program.parse();
 
-const options = program.opts();
-
-if (options.help) {
-    const textHelp = `Usage: gendiff [options]
-
-Compares two configuration files and shows a difference.
-
-Options:
-  -V, --version        output the version number
-  -f, --format [type]  output format
-  -h, --help           output usage information`;
-    
-    console.log(textHelp);
-}
+program.parse()
